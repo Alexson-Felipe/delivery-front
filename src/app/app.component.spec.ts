54e3daa428bcd494e96b6d17af59fd4a28e9cc98
Alexson Felipe <alexfelipesilveira@gmail.com>
2022-06-24 17:36:17 -0300

    Adicionando Angular/Material

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'devivery-front'`, () => {
+  it(`should have as title 'delivery-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('devivery-front');
+    expect(app.title).toEqual('delivery-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('devivery-front app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('delivery-front app is running!');
   });
 });
